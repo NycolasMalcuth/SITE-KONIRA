04acb92b8c0a613ebf18f472505a8bee23900783
Nycolas Malcuth <159721238+NycolasMalcuth@users.noreply.github.com>
2025-12-29 19:57:58 -0300

    Add 'sobre' route to Flask application

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/sobre')
+def sobre_nos():
+    return render_template('sobre_nos.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
